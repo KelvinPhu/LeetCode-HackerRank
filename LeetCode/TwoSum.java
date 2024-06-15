0569f4eaf8cb0a05257b3ca053aa702c716230e7
huynh_thien_phu <phuhuynh197@gmail.com>
2024-06-15 10:07:29 +0700

    solution 2 using hash table

--- LeetCode/TwoSum.java
+++ LeetCode/TwoSum.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 1. Two Sum
@@ -36,11 +38,11 @@ import java.util.ArrayList;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] { i, j };
-                }
+        Map<Integer, Integer> result = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (result.containsKey(complement)) {
+                return new int[] { result.get(complement), nums[i] };
             }
         }
         return new int[] {};
